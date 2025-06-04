0688332bec809fcb7afe74aac3f1096b23f71ce1
LuigiM <jlpadilla@innovacion.gob.sv>
2025-06-04 14:19:13 -0600

    Fix: using v-img

--- src/components/ListProducts.vue
+++ src/components/ListProducts.vue
@@ -2,7 +2,6 @@
   <v-row>
     <v-col cols="12" md="4" v-for="dessert in desserts">
       <!-- {{ dessert }} -->
-      <!-- <Image img="../assets/images/image-baklava-desktop.jpg" /> -->
       <Image :img="dessert.image" />
     </v-col>
   </v-row>
